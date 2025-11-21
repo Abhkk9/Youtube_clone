9ec48df9a7d22279eb36b066d7c79830709aa17a
Abhkk9 <Abhshk.k98@gmail.com>
2025-11-21 23:35:05 +0530

    Add VideoCard component and implement video fetching in VideoContainer

---
+++ youtube/src/componets/VideoCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function VideoCard({data}) {
+    
+    console.log(data);
+    const {snippet, statistics} = data;
+    const {channelTitle,title,thumbnails }= snippet;
+    
+  return (
+    <div className=' p-2 m-2 w-80  shadow-md'>
+      <img className='rounded-lg' src ={thumbnails.medium.url} alt='Thumbnail'/>
+      <ul>
+        <li className='font-bold'>{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics.viewCount}</li>
+      </ul>
+        
+    </div>
+  )
+}
+
+export default VideoCard
